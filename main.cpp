3fbe9bd039edadf5871c6a2dc5cbd47951fc2d62
xkingtrey <tclarksonjr@gmail.com>
2018-03-17 02:21:20 -0500

    Test Run: commit updates

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!\n" << endl;
+    cout << " Test Run!" << endl;
     return 0;
 }
